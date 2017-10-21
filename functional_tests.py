499ac392c72ab3d1dbb38113e0f0a2cd6a525104
Che1 <nachwon@gmail.com>
2017-10-22 00:03:20 +0900

    functional_tests.py refactored

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 사용자가 새로운 'to-do list' 앱에 대해 알게됨.
         # to-do list 앱 홈페이지에 접속함.
@@ -40,7 +45,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 기타줄 갈기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 기타줄 갈기')
 
         # 다른 할일 목록을 추가할 수 있도록  텍스트 입력칸이 유지됨.
         # 사용자가 '피크 사기' 를 추가함.
@@ -52,11 +57,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1: 기타줄 갈기"
         # "2: 피크 사기"
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: 기타줄 갈기', [row.text for row in rows])
-        self.assertIn('2: 피크 사기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 기타줄 갈기')
+        self.check_for_row_in_list_table('2: 피크 사기')
 
         self.fail('Finish the test!')
 
