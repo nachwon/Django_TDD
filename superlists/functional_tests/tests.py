2b10cb6abe1d34d26ae4f338d5dcf14a8a659226
Che1 <nachwon@gmail.com>
2017-10-22 11:01:58 +0900

    functional_tests extended to check if new users get their own separated to-do lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,7 +3,6 @@ import time
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
-import unittest
 
 from selenium.webdriver.common.keys import Keys
 
@@ -44,9 +43,8 @@ class NewVisitorTest(LiveServerTestCase):
         # 사용자가 엔터를 치면, 페이지가 갱신되고, 입력한 할일 목록이 표시됨.
         # "1: 기타줄 갈기"
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, r'lists/.+')
         self.check_for_row_in_list_table('1: 기타줄 갈기')
 
         # 다른 할일 목록을 추가할 수 있도록  텍스트 입력칸이 유지됨.
@@ -58,14 +56,35 @@ class NewVisitorTest(LiveServerTestCase):
         # 페이지가 다시 갱신되고, 할일 목록에 추가한 항목이 표시됨.
         # "1: 기타줄 갈기"
         # "2: 피크 사기"
-
         self.check_for_row_in_list_table('1: 기타줄 갈기')
         self.check_for_row_in_list_table('2: 피크 사기')
 
+        # 새로운 사용자가 사이트에 접속함
+        ## 이전 사용자의 정보가 쿠키 등으로 새로운 사용자에 전달되지 않도록 하기 위해 새로운 브라우저를 사용함.
+        self.browser.quit()
+        self.browser = webdriver.Chrome('/home/che1/Projects/Django/django_tdd/chromedriver')
+
+        # 새로운 사용자가 홈페이지에 접속하면 이전 사용자의 리스트가 보이지 않음.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('1: 기타줄 갈기', page_text)
+        self.assertNotIn('2: 피크 사기', page_text)
+
+        # 새로운 사용자가 새로운 할 일 목록을 입력함.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 새로운 사용자는 자신만의 고유한 URL을 할당받음.
+        new_user_list_url = self.browser.current_url
+        self.assertIn(new_user_list_url, r'list/.+')
+        self.assertNotEqual(new_user_list_url, user_list_url)
+
+        # 이전 사용자의 목록이 없는지 재확인
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('1: 기타줄 갈기', page_text)
+        self.assertNotIn('2: 피크 사기', page_text)
+
+        # 두 사용자 모두 만족함
+
         self.fail('Finish the test!')
-
-        # 사용자가 이 목록을 저장하고 싶어하는 찰나에, 사이트에서 사용자를 위한 고유한 URL을 만들어 준 것을 발견함. (이 URL에 대한 설명이 달려있음)
-
-        # 사용자가 해당 URL에 접속하면 작성한 할일 목록이 저장되어 있음.
-
-        # 사용자는 만족하고 자러감.
